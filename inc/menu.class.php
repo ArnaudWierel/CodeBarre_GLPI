c7ddf88cca04554d0d1e8527b41a5c4193e98632
Arnaud WIEREL <arnaud.wierel@viacesi.fr>
2023-08-30 15:45:24 +0200

    configuration initale du plugin

---
+++ inc/menu.class.php
@@ -0,0 +1,10 @@
+<?php
+class PluginCodeBarre_GLPI_Menu extends CommonDropdown
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->setTitle('Code Barre');
+    }
+}
